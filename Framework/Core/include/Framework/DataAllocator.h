633e6a473ac95981196df67453b02603f7d4762c
Anton Alkin <anton.alkin@cern.ch>
2026-06-12 11:13:25 +0200

    fixup! DPL Analysis: use shm metadata in CCDB tables instead of binary view

--- Framework/Core/include/Framework/DataAllocator.h
+++ Framework/Core/include/Framework/DataAllocator.h
@@ -42,7 +42,10 @@
 
 // Do not change this for a full inclusion of fair::mq::Device.
 #include <fairmq/FwdDecls.h>
-#include <shmem/Message.h>
+#include <fairmq/Version.h>
+#if (FAIRMQ_VERSION_DEC >= 111000)
+#include <fairmq/shmem/Message.h>
+#endif
 
 namespace arrow
 {
@@ -604,9 +607,11 @@ DataAllocator::CacheId DataAllocator::adoptContainer(const Output& spec, Contain
     // clone is indistinguishable from the original, we can keep sending
     // the original.
     cacheId.value = context.addToCache(payloadMessage);
+#if (FAIRMQ_VERSION_DEC >= 111000)
     auto meta = dynamic_cast<fair::mq::shmem::Message*>(payloadMessage.get())->GetMeta();
     cacheId.handle = meta.fHandle;
     cacheId.segment = meta.fSegmentId;
+#endif
   }
 
   context.add<MessageContext::TrivialObject>(std::move(headerMessage), std::move(payloadMessage), routeIndex);
